487819710437a212a12fe2034bd628c84ad3660c
Chaxzer <110409740+Chaxzer@users.noreply.github.com>
2024-01-24 13:49:31 +0000

    ADDED ERROR HANDELING

--- script.js
+++ script.js
@@ -9,36 +9,48 @@ loadScript = (src, callback) => {
   script.onload = () => {
     console.log("Script loaded with src: " + src);
 
-    // Uncomment this block if you want to invoke the callback functiononly when it is a type of function
-    
+    // Uncomment this block if you want to invoke the callback functiononly when it is a type of function  
     // if (callback && typeof callback === "function") {
     //   callback();
     // }
-
+    
     // Pass the source as an argument to the callback function
-    callback(src);
-  };
+    callback(null, src);
+                        };
 
   
-  // can use this on error 
-  // script.onerror = () =>{}
-  //   console.log("Error loading script with src: " + src)}
+  //----------- can use this on error ---------
+  // Set up an onerror event handler for the script
+  script.onerror = () =>{
+    console.log("Error loading script with src: " + src)
+                        };
+  
+    callback(new Error("Src got some error"))
 
+
+    
   // Append the script element to the body of the HTML document
   document.body.appendChild(script);
-};
+                              };
 
 // Define a function to be used as a callback when the script is loaded
-create = (src) => {
-  // Prompt the user for a script name
-  let a = prompt("Enter the script name");
+create = (error, src) => {
+//RETURNS ERROR IN CASE OF ERROR
+  if (error){
+    console.log(error)
+    return
+            };
+      // Prompt the user for a script name
+      let a = prompt("Enter the script name");
+    
+      // Create an <h1> element with the entered name and append it to the body
+      document.body.appendChild(document.createElement("h1")).innerText = `My name is ${a}`;
+    
+      // Create a <p> element displaying the source site and append it to the body
+      document.body.appendChild(document.createElement("p")).innerText = `Source site is ${src}`;
+                        };       
 
-  // Create an <h1> element with the entered name and append it to the body
-  document.body.appendChild(document.createElement("h1")).innerText = `My name is ${a}`;
 
-  // Create a <p> element displaying the source site and append it to the body
-  document.body.appendChild(document.createElement("p")).innerText = `Source site is ${src}`;
-};
 
 // Load the script with the specified source and use the 'create' function as a callback
 loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', create);
